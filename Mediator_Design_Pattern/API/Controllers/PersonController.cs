50dcbdef0dbeaf1cc17cf1a94a2e1c088d8d99ed
hazaltugrul <93864451+hazaltugrul@users.noreply.github.com>
2024-01-22 01:05:20 +0300

    Get method completed

---
+++ Mediator_Design_Pattern/API/Controllers/PersonController.cs
@@ -0,0 +1,43 @@
+﻿using DemoLibrary.Models;
+using DemoLibrary.Models.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private IMediator _mediator;
+        public PersonController(IMediator mediator)
+        {
+           _mediator = mediator;
+        }
+
+
+        // GET: api/<PersonController>
+        [HttpGet]
+        public async Task<List<PersonModel>> Get()
+        {
+            return await _mediator.Send(new GetPersonListQuery());
+        }
+
+        // GET api/<PersonController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PersonController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+
+    }
+}
