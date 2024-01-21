b771e528a7165b1acc3284fecae7137e25d18d9b
hazaltugrul <93864451+hazaltugrul@users.noreply.github.com>
2024-01-22 00:39:30 +0300

    Displaying the data in blazor page is done

---
+++ Mediator_Design_Pattern/DemoLibrary/Models/Handlers/GetPersonListHandler.cs
@@ -0,0 +1,24 @@
+﻿using DemoLibrary.DataAccess;
+using DemoLibrary.Models.Queries;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.Models.Handlers
+{
+    public class GetPersonListHandler : IRequestHandler<GetPersonListQuery, List<PersonModel>>
+    {
+        private readonly IDataAccess _dataAccess;
+        public GetPersonListHandler(IDataAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+        }
+        public Task<List<PersonModel>> Handle(GetPersonListQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_dataAccess.GetPeople());
+        }
+    }
+}
