be2e6762dfa168e9b8f540b300e2560a48ad82b3
hazaltugrul <93864451+hazaltugrul@users.noreply.github.com>
2024-01-22 01:56:15 +0300

    Post method (insert )completed

--- Mediator_Design_Pattern/API/Controllers/PersonController.cs
+++ Mediator_Design_Pattern/API/Controllers/PersonController.cs
@@ -1,4 +1,5 @@
 ﻿using DemoLibrary.Models;
+using DemoLibrary.Models.Commands;
 using DemoLibrary.Models.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -34,8 +35,11 @@ namespace API.Controllers
 
         // POST api/<PersonController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<PersonModel> Post([FromBody] PersonModel value)
         {
+            //var model = new InsertPersonCommand(value.Name, value.LastName);  return await _mediator.Send(model);
+
+            return await _mediator.Send(new InsertPersonCommand(value.Name, value.LastName));
         }
 
 
---
+++ Mediator_Design_Pattern/DemoLibrary/Models/Handlers/InsertPersonHandler.cs
@@ -0,0 +1,26 @@
+﻿using DemoLibrary.DataAccess;
+using DemoLibrary.Models.Commands;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.Models.Handlers
+{
+    public class InsertPersonHandler : IRequestHandler<InsertPersonCommand, PersonModel>
+    {
+        private readonly IDataAccess _dataAccess;
+
+        public InsertPersonHandler(IDataAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+        }
+
+        public Task<PersonModel> Handle(InsertPersonCommand request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_dataAccess.InsertPerson(request.Name, request.LastName));
+        }
+    }
+}
