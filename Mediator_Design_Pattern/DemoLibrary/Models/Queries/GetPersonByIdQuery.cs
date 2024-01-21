3fdaa47252832579400343a7d0adf5f22fc421e8
hazaltugrul <93864451+hazaltugrul@users.noreply.github.com>
2024-01-22 01:21:39 +0300

    GetById method completed

---
+++ Mediator_Design_Pattern/DemoLibrary/Models/Queries/GetPersonByIdQuery.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.Models.Queries
+{
+    public class GetPersonByIdQuery: IRequest<PersonModel>
+    {
+        public int Id { get; set; }
+
+        public GetPersonByIdQuery(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+
+    //You can do the same with a record instead of a class
+
+    // public record GetPersonByIdQuery(int id): IRequest<PersonModel>;
+
+}
